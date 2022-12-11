155146bd9b8c04b128c13b5962bee498bf1a4a49
ophirus <dreckguy@gmail.com>
2022-12-11 20:56:41 +0200

    recieve form data

--- servers/website/index.js
+++ servers/website/index.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
 const port = 80
 
 app.use(express.static('public'))
+
+app.post('/request', function(req, res) {
+    let songRequest = req.body
+    console.log(songRequest)
+    res.sendFile(__dirname + '/public/requests.html')
+    
+});
 
 
 app.listen(port, () => {
