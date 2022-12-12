777b4497c97c751f6060ee5b2faa3297f43bf4a5
ophirus <dreckguy@gmail.com>
2022-12-12 14:53:15 +0200

    split screen

--- servers/website/index.js
+++ servers/website/index.js
@@ -1,3 +1,6 @@
+const {CloudTasksClient} = require('@google-cloud/tasks');
+const client = new CloudTasksClient();
+
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
