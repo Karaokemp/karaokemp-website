c479dd5161b6299f0db08cffc04e276d8b2024c1
ophirus <dreckguy@gmail.com>
2022-12-19 22:13:47 +0200

    add request time to element

--- servers/website/services/storage.js
+++ servers/website/services/storage.js
@@ -9,10 +9,12 @@ const bucket = storage.bucket(BUCKET_NAME)
  async function getKaraokeSongs(){
     let files = await bucket.getFiles()
     return files[0].map(file=>{
+        console.log(file)
         return {
             url: `https://storage.googleapis.com/karaokemp-karaoke-songs/${file.name}`,
             title: file.metadata.metadata.title,
-            requester: file.metadata.metadata.requester
+            requester: file.metadata.metadata.requester,
+            request_time: file.metadata.timeCreated
         }
     })
 }
